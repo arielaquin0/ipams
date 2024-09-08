43d0b00cd6f39465ddfa6e4418ed865851e21fd2
Ariel Aquino <arielaquino@outlook.ph>
2024-09-08 09:09:18 +0800

    Create users model and migration

---
+++ backend/app/Models/BaseModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Model;
+
+class BaseModel extends Model
+{
+    
+}
---
+++ backend/database/migrations/2024_09_08_085931_create_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('firstname', 50);
+            $table->string('lastname', 50);
+            $table->string('username', 50)->unique()->index();
+            $table->string('password', 65);
+            $table->string('password_salt', 4);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
